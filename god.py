10b8d5dcb4ba273a08f228684a247b3c080d4c87
Jackson Czarnecki <jacz8564@colorado.edu>
2025-01-18 01:02:48 +0000

    addded basic algos

--- god.py
+++ god.py
@@ -25,4 +25,5 @@ def processTrans(decision, open):
     if(decision == 2 and bitcoin > 0):
         money = bitcoin * open
         bitcoin = 0
+    print("Bitcoin: " + bitcoin + "Money: " + money)
         
